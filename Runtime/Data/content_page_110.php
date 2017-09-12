172044a4387250dee1db93bf8ac6717e4fbeaf56
root <root@host.localdomain>
2017-09-12 10:38:29 -0400

    11

---
+++ Runtime/Data/content_page_110.php
@@ -0,0 +1,52 @@
+a:10:{i:0;a:7:{s:2:"id";s:2:"32";s:13:"article_title";s:27:"刚一进屋，就看到...";s:15:"article_headimg";s:110:"
+<img src="//pic.qiushibaike.com/system/avtnew/2016/20164589/medium/20170619172130.JPEG" alt="黄山小妖" />";s:14:"article_author";s:12:"黄山小妖";s:15:"article_content";s:601:"
+
+刚一进屋，就看到老公坐在沙发上...我吓了一跳，马上说道：“你啥时候回来的？”老公淡定的坐在沙发上质问道：“几点了，你去哪了？”这时，儿子从屋里迷迷糊糊的走了出来，说道：“妈妈，故事书买回来了吗？”我笑着说道：“买回来了，我给你爸爸热一下饭，然后就过去给你讲...”老公说道：“我吃过了，你去给孩子讲故事哄他睡觉吧！”我带着儿子进了卧室，准备给儿子讲故事，儿子一把将书摔在一边，小声说道：“这是最后一次...”
+
+";s:20:"article_publish_time";s:10:"1505019918";s:11:"article_img";s:0:"";}i:1;a:7:{s:2:"id";s:2:"33";s:13:"article_title";s:27:"刚刚走在街上，突...";s:15:"article_headimg";s:104:"
+<img src="//pic.qiushibaike.com/system/avtnew/941/9410373/medium/20150806183450.jpg" alt="∵雄少" />";s:14:"article_author";s:9:"∵雄少";s:15:"article_content";s:130:"
+
+刚刚走在街上，突然眼前一亮，掏出手机！！！然后，，，，就没有然后了！亮点自己找！！！
+
+";s:20:"article_publish_time";s:10:"1505019922";s:11:"article_img";s:0:"";}i:2;a:7:{s:2:"id";s:2:"34";s:13:"article_title";s:27:"我喜欢阳光爱运动...";s:15:"article_headimg";s:124:"
+<img src="//pic.qiushibaike.com/system/avtnew/2477/24773850/medium/2017090715142534.JPEG" alt="阿莫西林有负作用" />";s:14:"article_author";s:24:"阿莫西林有负作用";s:15:"article_content";s:472:"
+
+我喜欢阳光爱运动的男生，尤其喜欢会踢球的男生，继阿迪耐克风波以后，我发了一个状态我喜欢爱踢球的男生，穿一身足球服就很能让我着迷，结果呢，第二天，我打篮球的同学，就跟我说，谢谢你啊，以前约篮球总是人不够，你一发状态，原来踢足球的都改打篮球了，大写的尴尬！以后你们竞争不过对手的时候就找我，我发发状态就能帮助到你们！
+
+";s:20:"article_publish_time";s:10:"1505019925";s:11:"article_img";s:0:"";}i:3;a:7:{s:2:"id";s:2:"35";s:13:"article_title";s:27:"今天我过生日，眼...";s:15:"article_headimg";s:122:"
+<img src="//pic.qiushibaike.com/system/avtnew/3131/31316777/medium/20170717090906.JPEG" alt="其实那年我也十八" />";s:14:"article_author";s:24:"其实那年我也十八";s:15:"article_content";s:277:"
+
+今天我过生日，眼看着到了晚上了家里人也没有给我打电话，我就给爸爸打过去了，听到那边气氛异常欢乐，爸爸说他和妈妈在姨妈家聚餐吃火锅呢！我说：“为什么啊？”老爸说：“今儿不是你生日嘛！”
+
+";s:20:"article_publish_time";s:10:"1505019930";s:11:"article_img";s:0:"";}i:4;a:7:{s:2:"id";s:2:"36";s:13:"article_title";s:27:"坐在沙发上看电视...";s:15:"article_headimg";s:114:"
+<img src="//pic.qiushibaike.com/system/avtnew/3385/33857472/medium/2017060612065173.JPEG" alt="喂~喂~喂！" />";s:14:"article_author";s:14:"喂~喂~喂！";s:15:"article_content";s:197:"
+
+坐在沙发上看电视，媳妇穿着性感蕾丝内衣走过来：“老公，你该交作业啦…” 我：“那个，媳妇！！我笔没有油了，要不你那个…自习吧……”
+
+";s:20:"article_publish_time";s:10:"1505019932";s:11:"article_img";s:0:"";}i:5;a:7:{s:2:"id";s:2:"37";s:13:"article_title";s:27:"那年三月我和老公...";s:15:"article_headimg";s:112:"
+<img src="//pic.qiushibaike.com/system/avtnew/3296/32966062/medium/2016112414593870.JPEG" alt="明月天天" />";s:14:"article_author";s:12:"明月天天";s:15:"article_content";s:728:"
+
+那年三月我和老公确定恋爱关系，五一他提议去青岛玩，那时候还不大流行从网上订酒店，所以我们是去了以后才现找的酒店，毕竟五一酒店不好找。先看到一个小旅馆，进去看了房间，我觉得还好，挺便宜，随便凑合一晚不就行了？他非说不干净再找别的。我就纳闷了，和衣躺下凑活睡一晚就行啊！那么累了还要走路再找。他一再坚持找了好久才找到一家全国连锁的快捷酒店有房间。好吧！那是我们的第一次啪。后来我问他是不是那次去青岛你是提前有预谋的？他笑而不答。那时候单纯的我，是被他被套路了啊[大哭][大哭][大哭][大哭][大哭]
+
+";s:20:"article_publish_time";s:10:"1505020135";s:11:"article_img";s:0:"";}i:6;a:7:{s:2:"id";s:2:"38";s:13:"article_title";s:27:"想着老公白天上班...";s:15:"article_headimg";s:134:"
+<img src="//pic.qiushibaike.com/system/avtnew/1333/13337951/medium/20170208233317.JPEG" alt="面朝大海、日到春暖花开..." />";s:14:"article_author";s:36:"面朝大海、日到春暖花开...";s:15:"article_content";s:402:"
+
+想着老公白天上班辛苦，医院待产时就没要求陪过床，一直都是自己一个人，白天用机器测的时候，隔壁床产妇的老公问我，你不痛么？我老婆宫缩50都哭的死去活来的，你这都快100了你怎么没反应？<br />我:痛啊，<br />他:那你怎么不哭？<br />我:我哭给你看么？[笑哭][笑哭]你老婆也不同意啊……[偷笑]
+
+";s:20:"article_publish_time";s:10:"1505020138";s:11:"article_img";s:0:"";}i:7;a:7:{s:2:"id";s:2:"39";s:13:"article_title";s:25:"路边手机随拍。
+...";s:15:"article_headimg";s:108:"
+<img src="//pic.qiushibaike.com/system/avtnew/967/9670515/medium/20170728162436.JPEG" alt="捧起明珠" />";s:14:"article_author";s:12:"捧起明珠";s:15:"article_content";s:25:"
+
+路边手机随拍。
+
+";s:20:"article_publish_time";s:10:"1505020140";s:11:"article_img";s:0:"";}i:8;a:7:{s:2:"id";s:2:"40";s:13:"article_title";s:27:"公司要拍集体照，...";s:15:"article_headimg";s:122:"
+<img src="//pic.qiushibaike.com/system/avtnew/2470/24703920/medium/20170908082026.JPEG" alt="醉酒舞剑砍疯子※" />";s:14:"article_author";s:24:"醉酒舞剑砍疯子※";s:15:"article_content";s:306:"
+
+公司要拍集体照，小丽傲娇的对说，姐，想当年我上学那会就是班花，在xx公司上班时，也是头牌，没想到跳槽到这个公司还是头牌！<br />旁边大刘说话了：得了吧你！就你那身高站后面拍照，别人也看不见你呀！<br />小丽。。。
+
+";s:20:"article_publish_time";s:10:"1505020143";s:11:"article_img";s:0:"";}i:9;a:7:{s:2:"id";s:2:"41";s:13:"article_title";s:27:"有的时候这个二傻...";s:15:"article_headimg";s:107:"
+<img src="//pic.qiushibaike.com/system/avtnew/649/6496396/medium/20160309142651.jpg" alt="姓王名后" />";s:14:"article_author";s:12:"姓王名后";s:15:"article_content";s:91:"
+
+有的时候这个二傻子也挺可爱的，人生处处是套路……防不胜防～
+
+";s:20:"article_publish_time";s:10:"1505020145";s:11:"article_img";s:0:"";}}
